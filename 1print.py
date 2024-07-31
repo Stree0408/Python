68688baf0090e5927c367bbb6e4253b0466f6f30
Stree0408 <146194500+Stree0408@users.noreply.github.com>
2024-07-31 11:51:02 +0900

    Add files via upload

---
+++ 1print.py
@@ -0,0 +1,67 @@
+# print('......')
+
+# 6
+# a = int(input('--->'))
+# b = int(input('--->'))
+# result = a + b 
+# print(a, "+", b, "=", result)
+# result = a - b
+# print(a, "-", b, "=", result)
+# result = a * b
+# print(a, "*", b, "=", result)
+# result = a / b
+# print(a, '/', b, "=", result)
+
+# print(1, end=' ')
+# print(20)
+
+# 7 
+# print(1,2,3)
+# print(1,2,3, sep='//')
+# print(1,2,3, sep='.', end='?')
+# print(1,2,3, sep='.', end='\n')
+# print(1,2,3, sep='.', end='!')
+
+
+# 8 
+# \n :new line  \t:tab   \b:back space  \": "출력
+
+# print("sdaklak' ajlasdgk")
+# print(" 하나 \n 둘 ")
+# print(" 하나 \t 둘 ")
+# print(" 하나 \ 둘 ")
+# print(" 하나 \" 둘 ")
+# print(" 하나\b 둘 ")
+
+# + - * //:정수형 나누기 10//3 --> 3   /: 실수형 나누기 10/3 -->3.33333   %:나머지
+# 22222
+# print('A','B',"C")
+# print('A'+'B'+'C')
+
+# 2) 11 을 4로 나누면?, 2.75 나머지는? 3 
+
+
+# Question 3
+# c:
+#  name
+# seoul   korea
+# beep\a\a\a ???????????????????
+
+
+# Question 4 
+# print('==>메뉴를 골라 보세요')
+# print('\n\n1. burger')
+# print('2. set menu')
+# print('3. mc lunch')
+# print('4.', 'Mc morning')
+
+# Question 5 
+# print('  <총점과 평균 계산>\n')
+# print('=====================\n')
+# print(' 국어 : 80\n')
+# print(' 영어 : 85\n')
+# print('=====================\n')
+# print(' 총점 : 165 평균 : 82.5')
+
+# Question 6 
+# print('내이름은 김하나이고\n나이는 16살이고\n나는 미금중학교에 다녀')
